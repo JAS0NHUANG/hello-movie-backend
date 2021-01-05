6a3aa5389bb0f2693534c6c4f389b735b0925da1
Jason HUANG <jahuang@e1r8p14.42.fr>
2021-01-05 15:43:15 +0100

    add unsubscribe

--- controllers/email.js
+++ controllers/email.js
@@ -3,17 +3,30 @@ const dbClient = require('../models/dbClient');
 const emailController = {
   subscribeEmail: async (request, response) => {
     const { email } = request.body;
-    if (!email) return response.json({ ok: 0, errorMessage: 'Email value required.' });
+    if (!email) return response.json({ ok: 0, errorMessage: 'Email required.' });
     try {
       const data = await dbClient.db('test').collection('emails')
         .updateOne({ email }, { $set: { email } }, { upsert: true });
       response.json(data.result);
-      return console.log('Data Sent');
+      return console.log('Email Sent');
     } catch (error) {
-      response.json({ ok: 0, errorMessage: 'Error' });
+      response.json({ ok: 0, errorMessage: error });
       return console.log(error);
     }
   },
+  unSubscribeEmail: async (request, response) => {
+    const emailStr = request.query.email;
+    try {
+      const data = await dbClient.db('test').collection('emails')
+        .deleteOne({ email });
+      response.json(data.result);
+      return console.log('Email Deleted');
+    } catch (error) {
+      response.json({ ok: 0, errorMessage: error });
+      return console.log(error);
+    }
+  },
+
 };
 
 module.exports = emailController;
